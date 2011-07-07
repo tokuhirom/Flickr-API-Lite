838087e68512814eac8f6cbc80151f0d046b9fb1
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-07-07 17:16:52 +0900

    initial import

---
+++ lib/Flickr/API/Lite.pm
@@ -0,0 +1,39 @@
+package Flickr::API::Lite;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Flickr::API::Lite -
+
+=head1 SYNOPSIS
+
+  use Flickr::API::Lite;
+
+=head1 DESCRIPTION
+
+Flickr::API::Lite is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
